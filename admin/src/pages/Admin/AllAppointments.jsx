5422bf669d70aa3cd6281a8b25ff4dd5e6953ccd
yash-shende99 <yashshende9999@gmail.com>
2025-12-10 00:19:14 +0530

    feat(views): scaffold AllAppointments.jsx page layout

---
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { use } from 'react'
+import { useEffect } from 'react'
+import { AppContext } from '../../context/AppContext'
+import { assets } from '../../assets/assets'
+
+
+const AllAppointments = () => {
+  const { aToken, appointment, getAllappointments, cancleAppointment } = useContext(AdminContext)
+  const { calculateAge, currency, slotDateFormat } = useContext(AppContext)
+
+  useEffect(() => {
+    if (aToken) {
+      getAllappointments()
+    }
+
+  }, [aToken])
+
+
+  return (
+    <div className='w-full max-w-6xl m-5'>
+      <p className='mb-3 text-lg font-medium'>All Appointments</p>
+
+      <div className='bg-white border border-gray-300 rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+        <div className='hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b border-gray-300'>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+
+        {
+          appointment.reverse().map((item, index) => (
+            <div className='flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500  py-3 px-6 border-b border-gray-300 hover: bg-gray-50' key={index}>
+              <p className='max-sm:hidden'>{index + 1}</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-8 rounded-full' src={item.userData.image} alt="" />
+                <p>{item.userData.name}</p>
+              </div>
+              <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
+              <p>{slotDateFormat(item.slotDate)} , {item.slotTime}</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-8 rounded-full bg-gray-200' src={item.docData.image} alt="" />
+                <p>{item.docData.name}</p>
+              </div>
+              <p>{currency}{item.amount}</p>
+              {
+                item.cancelled
+                  ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+                  : item.isCompleted
+                    ? <p className='text-green-400 text-xs font-medium'>Completed</p>
+                    : <img onClick={() => cancleAppointment(item._id)} src={assets.cancel_icon} alt="" />
+              }
+
+            </div>
+          ))
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default AllAppointments
